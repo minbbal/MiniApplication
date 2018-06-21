57b189b6e4cfb406593579d51dd9e9071348e591
minbbal <jashely@naver.com>
2018-06-21 10:28:36 +0900

    Initial commit

---
+++ app/src/main/java/com/minbbal/miniapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.minbbal.miniapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = findViewById(R.)
+
+
+    }
+}
